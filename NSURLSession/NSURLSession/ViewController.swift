706a962ee28104c467c7908290e6f69386c9fafc
kangkai <kangkai1108@sina.cn>
2021-05-12 18:25:54 +0800

    URLSessionDataT

--- NSURLSession/NSURLSession/ViewController.swift
+++ NSURLSession/NSURLSession/ViewController.swift
@@ -11,8 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        getRequest()
+        postReqeust()
     }
+    
+    var responseData = Data()
 }
 
 extension ViewController {
@@ -47,20 +49,46 @@ extension ViewController {
     
     func getRequest() {
         let sessionConfiguration = URLSessionConfiguration.default
-        sessionConfiguration.timeoutIntervalForRequest = 15
-        sessionConfiguration.timeoutIntervalForResource = 15
-        let session = URLSession.init(configuration: sessionConfiguration, delegate: self, delegateQueue: .current)
+//        let session = URLSession.init(configuration: sessionConfiguration, delegate: self, delegateQueue: .current)
 //        let dataTask = session.dataTask(with: URL.init(string: "https://www.baidu.com")!) { data, response, error in
-//            print(String.init(data: data!, encoding: .utf8))
+//            guard let data = data else {
+//                print(error?.localizedDescription ?? "")
+//                return
+//            }
+//            print(String.init(data: data, encoding: .utf8)!)
 //        }
-        let dataTask = session.dataTask(with: URL.init(string: "https://www.jd.com")!)
-        
+        let session = URLSession.init(configuration: sessionConfiguration, delegate: self, delegateQueue: .current)
+        let dataTask = session.dataTask(with: URL.init(string: "https://www.baidu.com")!)
         dataTask.resume()
+        
+        session.getTasksWithCompletionHandler { dataTasks, uploadTasks, downloadTasks in
+            print("all dataTaks ----> \(dataTasks)")
+        }
+    }
+    
+    func postReqeust() {
+        let session = URLSession.shared
+        var request = URLRequest.init(url: .init(string: "http://192.168.7.12/pension-service-api/api/staff/login")!)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = ["Content-Type": "application/json"]
+        
+        let bodyDic = ["type":"1","tel":"13688421393","password":"123456"]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: bodyDic, options: .fragmentsAllowed)
+        
+        let postTask = session.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            print(String.init(data: data, encoding: .utf8)!)
+        }
+        postTask.resume()
     }
 }
 
 extension ViewController: URLSessionDataDelegate {
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        responseData.append(data)
         print("recive data ----> \(String(describing: String.init(data: data, encoding: .utf8)))")
     }
     
@@ -87,6 +115,19 @@ extension ViewController: URLSessionDataDelegate {
     
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, willCacheResponse proposedResponse: CachedURLResponse, completionHandler: @escaping (CachedURLResponse?) -> Void) {
         print("will cache response ----> \(proposedResponse)")
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let err = error {
+            print(err.localizedDescription)
+            return
+        }
+        print(String.init(data: responseData, encoding: .utf8)!)
+    }
+    
+    /// 重定向
+    func urlSession(_ session: URLSession, task: URLSessionTask, willPerformHTTPRedirection response: HTTPURLResponse, newRequest request: URLRequest, completionHandler: @escaping (URLRequest?) -> Void) {
+        completionHandler(request)
     }
 }
 
