28970d387d24cd734f4638ff534d15d2b449ab31
kangkai <kangkai1108@sina.cn>
2021-05-28 17:53:35 +0800

    runloop

--- RunLoop/RunLoop/RunLoop/ViewController.swift
+++ RunLoop/RunLoop/RunLoop/ViewController.swift
@@ -9,28 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var newThread: Thread!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         launchNewThread()
     }
-    
-    func launchNewThread() {
-        let thread = Thread.init(target: self, selector: #selector(threadSelector), object: nil)
-        thread.name = "custom thread"
-        thread.start()
+    @IBAction func runCustomThreadSelector(_ sender: Any) {
+        perform(#selector(newThreadSelector), on: newThread, with: nil, waitUntilDone: true)
+        perform(<#T##aSelector: Selector##Selector#>, with: <#T##Any?#>, afterDelay: <#T##TimeInterval#>)
     }
     
+    /// 创建新线程，并执行任务
+    /// 正常情况下，线程执行完任务后，后自动退出
+    /// 开启线程的RunLoop可以保持线程的活性，处理新事件
+    func launchNewThread() {
+        newThread = Thread.init(target: self, selector: #selector(threadSelector), object: nil)
+        newThread.name = "custom thread"
+        newThread.start()
+    }
+    
+    // 开启新线程的RunLoop
     @objc private func threadSelector() {
         print("task start")
-        print(Date())
-        let date = RunLoop.current.limitDate(forMode: .default)
-        print(date)
         RunLoop.current.add(Port.init(), forMode: .default)
         RunLoop.current.run()
         print("task end")
-        
-        CFRunLoop
     }
     
+    // 在新线程上执行另一个任务
+    @objc private func newThreadSelector() {
+        print("new task start")
+        print(Thread.current)
+        print("new task end")
+    }
 }
 
