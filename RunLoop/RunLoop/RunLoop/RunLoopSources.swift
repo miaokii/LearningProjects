a1581012a9b3e1aadb18127069333d6743eef785
kangkai <kangkai1108@sina.cn>
2021-05-31 18:03:27 +0800

    runloop

---
+++ RunLoop/RunLoop/RunLoop/RunLoopSources.swift
@@ -0,0 +1,37 @@
+//
+//  RunLoopSources.swift
+//  RunLoop
+//
+//  Created by miaokii on 2021/5/31.
+//
+
+import Foundation
+
+class CustomRunLoopSource: NSObject {
+    var source: CFRunLoopSource!
+    var commands = [String]()
+    
+    override init() {
+        
+    }
+    
+    func addToCurrentRunLoop() {
+        
+    }
+    
+    func inValidate() {
+        
+    }
+    
+    func sourceFired() {
+        
+    }
+    
+    func add(command: Int, with data: Data)  {
+        
+    }
+    
+    func fireAllCompandsSource(on runLoop: CFRunLoop) {
+        
+    }
+}
